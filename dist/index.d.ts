5ebb6eb35884f8e08818881658746c89dc805be5
Aaron Gray <aaronngray@gmail.com>
2021-09-18 01:32:29 +0100

    Added 'dist' directory.

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="zen-observable" />
+import { ApolloLink, Operation, FetchResult, Observable } from '@apollo/client/core';
+import { ClientOptions } from 'graphql-ws';
+export default class WebSocketLink extends ApolloLink {
+    private client;
+    constructor(options: ClientOptions);
+    request(operation: Operation): Observable<FetchResult>;
+}
